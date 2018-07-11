1df751949d7a21c73ae07f2ad83b43d6c7d7f33c
Salah <salah@enterprise.cm.cluster>
2018-07-11 11:45:33 -0400

    update

--- ligand_binding_sites/clean.sh
+++
@@ -1,9 +0,0 @@
-for i in {10..50}
-do
-    echo "clean frame_$i"
-    cd frame_$i
-    rm acc.* debug.log energies.opp energies.opp error.log head1.lst head2.lst new.tpl progress.log respair.lst rot_stat run.trace step* vdw0.lst
-    cd binding
-    rm energies.opp entropy.out error.log pK.out respair.lst run.trace
-    cd ../..
-done
--- ligand_binding_sites/model_select_Cterm_RYR.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-while read -r LINE
-do
-echo "$LINE"
-awk 'NR>1 && NR<1113; NR>52333 && NR<60207; NR==122653' "$LINE"_5t9v_class1_ca_atp_cff-fit-coot-0_real_space_refined.pdb > "$LINE"_5t9v_class1_ca_atp_cff.pdb
-done < list
