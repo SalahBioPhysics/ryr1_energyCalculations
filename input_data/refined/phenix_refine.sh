79d5c5eced5688b15e271aae6584d3a74b27b62c
Salah <salah@enterprise.cm.cluster>
2018-07-11 13:21:21 -0400

    update

--- input_data/refined/phenix_refine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# for every item matching pattern supplied on command line do the following:
-for i in "$@"
-do
-phenix.real_space_refine ${i}_5t9r_norm_psi_Class50_SVD12_bfac290_4.5A.pdb ../maps/norm_psi_Class50_SVD12_${i}_bfac290_4.5A.mrc resolution=5 write_all_states=false write_initial_geo_file=False  
-done
--- input_data/refined/runphenix_refine.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-while read -r LINE
-do
-echo "$LINE"
-bash phenix_refine.sh $LINE >>model_refine.txt
-done < list1.txt
