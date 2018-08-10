6aa79d0d498c0b3937ba61be9e2e1685d882d177
Guest <guest@master.cl.cuny.edu>
2018-08-10 13:36:16 -0400

    data

--- output_data/move.sh
+++ output_data/move.sh
@@ -1,9 +1,9 @@
 for i in {1..9}
 do
-    cp /home/guest/ryr1_energyCalculations/calculations/5t9r/frame_0$i/fort.38 /home/guest/ryr1_energyCalculations/output_data/frame_0$i/
+    cp /home/guest/ryr1_energyCalculations/calculations/5t9r/frame_0$i/run.log /home/guest/ryr1_energyCalculations/output_data/frame_0$i/
 done
 
 for i in {10..50}
 do
-    cp /home/guest/ryr1_energyCalculations/calculations/5t9r/frame_$i/fort.38 /home/guest/ryr1_energyCalculations/output_data/frame_$i/
+    cp /home/guest/ryr1_energyCalculations/calculations/5t9r/frame_$i/run.log /home/guest/ryr1_energyCalculations/output_data/frame_$i/
 done
